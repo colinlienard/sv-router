d4c3ff32c863500d889a80e1e4e0e2b3486e2daf
Colin Lienard <colin.lienard87@gmail.com>
2024-12-01 22:45:27 +0100

    wip

--- src/vite-plugin/generate-routes.test.ts
+++ src/vite-plugin/generate-routes.test.ts
@@ -1,26 +1,56 @@
-import { generateRoutes } from './generate-routes.ts';
+import { generateRouter, generateRoutes } from './generate-routes.ts';
 
 describe('generateRoutes', () => {
 	it('should generate routes', () => {
-		expect(generateRoutes()).toBe(`
-import { createRouter } from 'sv-router';
-import DynamicPost from './routes/DynamicPost.svelte';
-import Home from './routes/Home.svelte';
-import NotFound from './routes/NotFound.svelte';
-import Posts from './routes/Posts.svelte';
-import StaticPost from './routes/StaticPost.svelte';
+		expect(
+			generateRoutes([
+				'index.svelte',
+				'about.svelte',
+				{
+					name: 'posts',
+					tree: ['index.svelte', 'static.svelte', ':id.svelte'],
+				},
+				'*.svelte',
+			]),
+		).toEqual({
+			'/': 'index.svelte',
+			'/about': 'about.svelte',
+			'/posts': {
+				'/': 'posts/index.svelte',
+				'/static': 'posts/static.svelte',
+				'/:id': 'posts/:id.svelte',
+			},
+			'*': '*.svelte',
+		});
+	});
+});
+
+describe('generateRouter', () => {
+	it('should generate the router', () => {
+		expect(
+			generateRouter(
+				[
+					'index.svelte',
+					'about.svelte',
+					{
+						name: 'posts',
+						tree: ['index.svelte', 'static.svelte', ':id.svelte'],
+					},
+					'*.svelte',
+				],
+				'./routes/',
+			),
+		).toBe(`import { createRouter } from "sv-router";
 
 export const { path, goto, params } = createRouter({
-	'/': Home,
-	'/about': () => import('./routes/About.svelte'),
-	'/posts': {
-		'/': Posts,
-		'/static': StaticPost,
-		'/:id': DynamicPost,
-		layout: () => import('./Layout.svelte'),
-	},
-	'*': NotFound,
-});
-      `);
+  "/": () => import("./routes/index.svelte"),
+  "/about": () => import("./routes/about.svelte"),
+  "/posts": {
+    "/": () => import("./routes/posts/index.svelte"),
+    "/static": () => import("./routes/posts/static.svelte"),
+    "/:id": () => import("./routes/posts/:id.svelte"),
+  },
+  "*": () => import("./routes/*.svelte"),
+});`);
 	});
 });
--- src/vite-plugin/generate-routes.ts
+++ src/vite-plugin/generate-routes.ts
@@ -1,1 +1,41 @@
-export function generateRoutes() {}
+type FileTree = (string | { name: string; tree: FileTree })[];
+
+type Result = {
+	[key: string]: string | Result;
+};
+
+export function generateRoutes(fileTree: FileTree, prefix = ''): Result {
+	const result: Result = {};
+	for (const entry of fileTree) {
+		if (typeof entry === 'string') {
+			switch (entry) {
+				case 'index.svelte': {
+					result['/'] = prefix + entry;
+					break;
+				}
+				case '*.svelte': {
+					result['*'] = prefix + entry;
+					break;
+				}
+				default: {
+					result['/' + entry.replace('.svelte', '')] = prefix + entry;
+					break;
+				}
+			}
+		} else {
+			result['/' + entry.name] = generateRoutes(entry.tree, prefix + entry.name + '/');
+		}
+	}
+	return result;
+}
+
+export function generateRouter(fileTree: FileTree, path: string) {
+	const routes = generateRoutes(fileTree);
+	const ok = JSON.stringify(routes, undefined, 2);
+	const withImports = ok.replaceAll(/"(.*)": "(.*)",?/g, `"$1": () => import("${path}$2"),`);
+	return [
+		'import { createRouter } from "sv-router";',
+		'\n\n',
+		`export const { path, goto, params } = createRouter(${withImports});`,
+	].join('');
+}
