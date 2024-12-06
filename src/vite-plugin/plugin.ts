af63d4ab7aaefac03c879f735fdb7604616608c7
Colin Lienard <colin.lienard87@gmail.com>
2024-12-06 19:25:22 +0100

    generate routes on postinstall

--- src/vite-plugin/plugin.ts
+++ src/vite-plugin/plugin.ts
@@ -1,13 +1,7 @@
-import fs from 'node:fs';
 import path from 'node:path';
 import type { Plugin } from 'vite';
-import { generateRouterCode } from '../gen/generate-router-code.ts';
-
-const ROUTES_PATH = 'src/routes';
-const GEN_CODE_DIR_PATH = '.router';
-const ROUTER_PATH = path.join(GEN_CODE_DIR_PATH, '/router.ts');
-const TSCONFIG_PATH = path.join(GEN_CODE_DIR_PATH, '/tsconfig.json');
-const GEN_CODE_ALIAS = 'sv-router/generated';
+import { GEN_CODE_ALIAS, ROUTER_PATH, ROUTES_PATH } from '../common.ts';
+import { writeRouterCode } from '../gen/write-router-code.ts';
 
 export function router(): Plugin {
 	return {
@@ -37,31 +31,4 @@ export function router(): Plugin {
 			}
 		},
 	};
-}
-
-function writeRouterCode() {
-	if (!fs.existsSync(GEN_CODE_DIR_PATH)) {
-		fs.mkdirSync(GEN_CODE_DIR_PATH);
-	}
-
-	const routerCode = generateRouterCode(ROUTES_PATH);
-	writeFileIfDifferent(ROUTER_PATH, routerCode);
-
-	const tsConfig = {
-		compilerOptions: {
-			module: 'Preserve',
-			moduleResolution: 'Bundler',
-			paths: {
-				[GEN_CODE_ALIAS]: [path.join('..', ROUTER_PATH)],
-			},
-		},
-		include: ['./router.ts'],
-	};
-	writeFileIfDifferent(TSCONFIG_PATH, JSON.stringify(tsConfig, undefined, 2));
-}
-
-function writeFileIfDifferent(filePath: string, content: string) {
-	if (!fs.existsSync(filePath) || fs.readFileSync(filePath, 'utf8') !== content) {
-		fs.writeFileSync(filePath, content);
-	}
 }
