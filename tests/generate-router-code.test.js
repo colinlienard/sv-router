0a53ec618a32da24f54e18b44208f8e3e8d5de0d
Colin Lienard <colin.lienard87@gmail.com>
2025-01-15 21:02:58 +0100

    feat(file-based): add support for flat mode

--- tests/generate-router-code.test.js
+++ tests/generate-router-code.test.js
@@ -5,10 +5,26 @@ import {
 	generateRouterCode,
 } from '../src/gen/generate-router-code.js';
 
-vi.mock('node:fs', () => ({
-	default: {
-		/** @param {string} dir */
-		readdirSync: (dir) => {
+const readdirSync = vi.hoisted(() => vi.fn());
+const lstatSync = vi.hoisted(() => vi.fn());
+vi.mock('node:fs', () => ({ default: { readdirSync, lstatSync } }));
+
+describe.each(['flat', 'tree'])('%s', (mode) => {
+	beforeAll(() => {
+		readdirSync.mockImplementation((dir) => {
+			if (mode === 'flat') {
+				return [
+					'*.svelte',
+					'about.svelte',
+					'index.svelte',
+					'posts.[id].svelte',
+					'posts._layout.svelte',
+					'posts.index.svelte',
+					'posts.static.svelte',
+					'posts.text.txt',
+					'posts.noextension',
+				];
+			}
 			if (dir.toString().endsWith('posts')) {
 				return [
 					'[id].svelte',
@@ -20,18 +36,17 @@ vi.mock('node:fs', () => ({
 				];
 			}
 			return ['*.svelte', 'about.svelte', 'index.svelte', 'posts'];
-		},
-		/** @param {string} dir */
-		lstatSync: (dir) => ({
-			isDirectory: () => dir.toString().endsWith('posts'),
-		}),
-	},
-}));
+		});
 
-describe('generateRouterCode', () => {
-	it('should generate the router code', () => {
-		const result = generateRouterCode('./a/fake/path');
-		expect(result).toBe(`import { createRouter } from "sv-router";
+		lstatSync.mockImplementation((dir) => ({
+			isDirectory: () => (mode === 'flat' ? false : dir.toString().endsWith('posts')),
+		}));
+	});
+
+	describe('generateRouterCode', () => {
+		it('should generate the router code', () => {
+			const result = generateRouterCode('./a/fake/path');
+			expect(result).toBe(`import { createRouter } from "sv-router";
 
 export const { path, goto, params } = createRouter({
   "*": () => import("../a/fake/path/*.svelte"),
@@ -44,65 +59,65 @@ export const { path, goto, params } = createRouter({
     "/static": () => import("../a/fake/path/posts/static.svelte"),
   }
 });`);
-	});
-});
-
-describe('buildFileTree', () => {
-	it('should get the file tree', () => {
-		const result = buildFileTree('a/fake/path');
-		expect(result).toEqual([
-			'*.svelte',
-			'about.svelte',
-			'index.svelte',
-			{
-				name: 'posts',
-				tree: ['[id].svelte', '_layout.svelte', 'index.svelte', 'static.svelte'],
-			},
-		]);
-	});
-});
-
-describe('createRouteMap', () => {
-	it('should generate routes', () => {
-		const result = createRouteMap([
-			'index.svelte',
-			'about.svelte',
-			{
-				name: 'posts',
-				tree: ['index.svelte', 'static.svelte', '[id].svelte', '_layout.svelte'],
-			},
-			'*.svelte',
-		]);
-		expect(result).toEqual({
-			'/': 'index.svelte',
-			'/about': 'about.svelte',
-			'/posts': {
-				'/': 'posts/index.svelte',
-				'/static': 'posts/static.svelte',
-				'/:id': 'posts/[id].svelte',
-				layout: 'posts/_layout.svelte',
-			},
-			'*': '*.svelte',
 		});
 	});
-});
 
-describe('createRouterCode', () => {
-	it('should generate the router', () => {
-		const result = createRouterCode(
-			{
+	describe('buildFileTree', () => {
+		it('should get the file tree', () => {
+			const result = buildFileTree('a/fake/path');
+			expect(result).toEqual([
+				'*.svelte',
+				'about.svelte',
+				'index.svelte',
+				{
+					name: 'posts',
+					tree: ['[id].svelte', '_layout.svelte', 'index.svelte', 'static.svelte'],
+				},
+			]);
+		});
+	});
+
+	describe('createRouteMap', () => {
+		it('should generate routes', () => {
+			const result = createRouteMap([
+				'index.svelte',
+				'about.svelte',
+				{
+					name: 'posts',
+					tree: ['index.svelte', 'static.svelte', '[id].svelte', '_layout.svelte'],
+				},
+				'*.svelte',
+			]);
+			expect(result).toEqual({
 				'/': 'index.svelte',
 				'/about': 'about.svelte',
 				'/posts': {
 					'/': 'posts/index.svelte',
 					'/static': 'posts/static.svelte',
-					'/:id': 'posts/:id.svelte',
+					'/:id': 'posts/[id].svelte',
+					layout: 'posts/_layout.svelte',
 				},
 				'*': '*.svelte',
-			},
-			'./routes',
-		);
-		expect(result).toBe(`import { createRouter } from "sv-router";
+			});
+		});
+	});
+
+	describe('createRouterCode', () => {
+		it('should generate the router', () => {
+			const result = createRouterCode(
+				{
+					'/': 'index.svelte',
+					'/about': 'about.svelte',
+					'/posts': {
+						'/': 'posts/index.svelte',
+						'/static': 'posts/static.svelte',
+						'/:id': 'posts/:id.svelte',
+					},
+					'*': '*.svelte',
+				},
+				'./routes',
+			);
+			expect(result).toBe(`import { createRouter } from "sv-router";
 
 export const { path, goto, params } = createRouter({
   "/": () => import("./routes/index.svelte"),
@@ -114,5 +129,6 @@ export const { path, goto, params } = createRouter({
   },
   "*": () => import("./routes/*.svelte"),
 });`);
+		});
 	});
 });
