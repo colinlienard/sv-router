1d5e8e4635e929ed62c51b66c207efda0f994f64
Colin Lienard <colin.lienard87@gmail.com>
2025-04-19 18:56:43 +0200

    improve templates

--- create/templates/code-based/src/App.svelte
+++ create/templates/code-based/src/App.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import './router.ts';
 	import { Router } from 'sv-router';
 	import { p } from './router';
 </script>
-
-<h1>Welcome to sv-router</h1>
 
 <a href={p('/')}>Home</a>
 <a href={p('/about')}>About</a>
