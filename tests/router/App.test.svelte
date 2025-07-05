85c4d20bf933a43231ad984d65727259b798c365
Colin Lienard <colin.lienard87@gmail.com>
2025-07-05 14:22:53 +0200

    test: add environment tests

---
+++ tests/router/App.test.svelte
@@ -0,0 +1,14 @@
+<script module lang="ts">
+	import { createRawSnippet } from 'svelte';
+	import { createRouter } from '../../src/create-router.svelte.js';
+	import Router from '../../src/Router.svelte';
+	import Layout from './Layout.test.svelte';
+
+	export const { p, navigate, isActive, preload, route } = createRouter({
+		'/': createRawSnippet(() => ({ render: () => '<h1>Welcome</h1>' })),
+		'/about': createRawSnippet(() => ({ render: () => '<h1>About Us</h1>' })),
+		layout: Layout,
+	});
+</script>
+
+<Router />
