22cb2b23be71aaa68a8f8fc8105602c188e66904
Colin Lienard <colin.lienard87@gmail.com>
2025-08-23 19:16:55 +0200

    fix: page remounted during view transitions

--- src/RecursiveComponentTree.svelte
+++ src/RecursiveComponentTree.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { params } from './create-router.svelte.js';
 	import RecursiveComponentTree from './RecursiveComponentTree.svelte';
 
 	/** @type {{ tree: import('svelte').Component[] }} */
@@ -9,10 +8,8 @@
 	const restTree = $derived(tree.slice(1));
 </script>
 
-{#key restTree.length > 0 || Object.values(params.value)}
-	<FirstComponent>
-		{#if restTree.length > 0}
-			<RecursiveComponentTree tree={restTree}></RecursiveComponentTree>
-		{/if}
-	</FirstComponent>
-{/key}
+<FirstComponent>
+	{#if restTree.length > 0}
+		<RecursiveComponentTree tree={restTree}></RecursiveComponentTree>
+	{/if}
+</FirstComponent>
---
+++ tests/router/UserPage.test.svelte
@@ -0,0 +1,5 @@
+<script>
+	import { route } from './App.test.svelte';
+</script>
+
+<h1>User page {route.params.id}</h1>
